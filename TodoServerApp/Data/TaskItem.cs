fd36ceed91c28523bf0a266f7b331159d04c7aa0
SysoevDA <fuzzii2739@gmail.com>
2024-09-16 21:48:36 +0500

    3

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedData { get; set; }
+        public DateTime? FinishData { get; set; }
+    }
+}
